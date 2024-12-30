af38c2d79571d034d13b72889a9b74fbde8f9d6f
Erick0330 <abdielrl0330@gmail.com>
2024-12-30 12:23:57 -0500

    Arreglada ruta signin

--- src/app/services/jwt.service.ts
+++ src/app/services/jwt.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -13,8 +13,26 @@ export class JwtService {
 
   constructor(private http: HttpClient) { }
 
+    private createAuthorizationHeader() {
+      const jwtToken = localStorage.getItem('jwt');
+      if(jwtToken){
+        console.log("JWT token found in local storage ", jwtToken)
+        return new HttpHeaders().set(
+          "Authorization", "Bearer " + jwtToken
+        )
+      }
+      else{
+        console.log("JWT token not found en local storage");
+      }
+
+      return new HttpHeaders();
+    }
+
+
   register(signRequest:any):Observable<any>{
-    return this.http.post(this.api + 'signup', signRequest)
+    return this.http.post(this.api + 'admin/signup', signRequest, {
+      headers: this.createAuthorizationHeader()
+    })
   }
 
   login(loginRequest:any):Observable<any>{
